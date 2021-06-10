c926b97a3c8cedf46d4ae6284e578252f36cb068
Henry Doan <henrydoan96@gmail.com>
2021-06-10 15:47:49 -0600

    Table view and delegate

---
+++ Table View Demo/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Table View Demo
+//
+//  Created by Henry Doan on 6/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "This is the cell for \(indexPath.row)"
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Table View DemoTests/Table_View_DemoTests.swift
@@ -0,0 +1,33 @@
+//
+//  Table_View_DemoTests.swift
+//  Table View DemoTests
+//
+//  Created by Henry Doan on 6/10/21.
+//
+
+import XCTest
+@testable import Table_View_Demo
+
+class Table_View_DemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
